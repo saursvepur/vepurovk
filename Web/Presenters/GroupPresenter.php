5a8e82a2e75af09a7af7e7b32318d0e748e379dc
Slava Petrov <saursvepur@yandex.ru>
2023-08-10 20:10:48 +0300

    Remove repeated code, fix loaded buttons on chrome and fix getting suggested posts via API.Wall.getPost
    Co-Authored-By: lalka2018 <99399973+lalka2016@users.noreply.github.com>

--- Web/Presenters/GroupPresenter.php
+++ Web/Presenters/GroupPresenter.php
@@ -477,67 +477,36 @@ final class GroupPresenter extends OpenVKPresenter
         $this->redirect("/club".$club->getId());
     }
 
-    function renderSuggestedThisUser(int $id)
+    function renderSuggested(int $id): void
     {
         $this->assertUserLoggedIn();
 
         $club = $this->clubs->get($id);
-        if(!$club || method_exists($club, "isDeleted") && $club->isDeleted())
+        if(!$club)
             $this->notFound();
         else
             $this->template->club = $club;
 
-        if($club->getWallType() == 1) {
+        if($club->getWallType() == 0) {
             $this->flash("err", tr("error_suggestions"), tr("error_suggestions_closed"));
             $this->redirect("/club".$club->getId());
         }
-
-        if($club->getWallType() == 0) {
-            $this->flash("err", tr("error_suggestions"), tr("error_suggestions_open"));
-            $this->redirect("/club".$club->getId());
-        }
-
-        if($club->canBeModifiedBy($this->user->identity)) {
-            $this->flash("err", tr("error_suggestions"), "No sense");
-            $this->redirect("/club".$club->getId());
-        }
-
-        $this->template->posts = (new Posts)->getSuggestedPostsByUser($club->getId(), $this->user->id, (int) ($this->queryParam("p") ?? 1));
-        $this->template->count = (new Posts)->getSuggestedPostsCountByUser($club->getId(), $this->user->id);
-        $this->template->type  = "my";
-        $this->template->page  = (int) ($this->queryParam("p") ?? 1);
-        $this->template->_template = "Group/Suggested.xml";
-    }
-
-    function renderSuggestedAll(int $id)
-    {
-        $this->assertUserLoggedIn();
-
-        $club = $this->clubs->get($id);
-        if(!$club || method_exists($club, "isDeleted") && $club->isDeleted())
-            $this->notFound();
-        else
-            $this->template->club = $club;
-
+    
         if($club->getWallType() == 1) {
-            $this->flash("err", tr("error_suggestions"), tr("error_suggestions_closed"));
-            $this->redirect("/club".$club->getId());
-        }
-
-        if($club->getWallType() == 0) {
             $this->flash("err", tr("error_suggestions"), tr("error_suggestions_open"));
             $this->redirect("/club".$club->getId());
         }
-
+        
         if(!$club->canBeModifiedBy($this->user->identity)) {
-            $this->flash("err", tr("error_suggestions"), tr("error_suggestions_access"));
-            $this->redirect("/club".$club->getId());
+            $this->template->posts = (new Posts)->getSuggestedPostsByUser($club->getId(), $this->user->id, (int) ($this->queryParam("p") ?? 1));
+            $this->template->count = (new Posts)->getSuggestedPostsCountByUser($club->getId(), $this->user->id);
+            $this->template->type  = "my";
+        } else {
+            $this->template->posts = (new Posts)->getSuggestedPosts($club->getId(), (int) ($this->queryParam("p") ?? 1));
+            $this->template->count = (new Posts)->getSuggestedPostsCount($club->getId());
+            $this->template->type  = "everyone";
         }
 
-        $this->template->posts = (new Posts)->getSuggestedPosts($club->getId(), (int) ($this->queryParam("p") ?? 1));
-        $this->template->count = (new Posts)->getSuggestedPostsCount($club->getId());
-        $this->template->type  = "everyone";
         $this->template->page  = (int) ($this->queryParam("p") ?? 1);
-        $this->template->_template = "Group/Suggested.xml";
     }
 }
